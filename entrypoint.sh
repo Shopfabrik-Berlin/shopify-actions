21aa2ec344090ddeb58ef5bad35dded72313e597
Max Less <maxless@Maxs-Air.fritz.box>
2020-09-09 14:47:02 +0200

    missing locale tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ if [ -n "$SHOPIFY_LOCALE" ]; then
     echo "locale defined"
     mkdir -p settings_locale/$SHOPIFY_LOCALE
     theme get --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH/settings_locale/$SHOPIFY_LOCALE config/settings_data.json $INPUT_ARGS -e production-$SHOPIFY_LOCALE && \
-    theme get --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH --ignored-file=config/settings_data.json $INPUT_ARGS -e production
+    theme get --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH --ignored-file=config/settings_data.json $INPUT_ARGS -e production-$SHOPIFY_LOCALE
 else
     echo "locale is empty"
     theme get --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $INPUT_ARGS -e production
