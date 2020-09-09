5f8fa06af9191a6214aaa60c26f100b0ba8a71f2
Max Less <maxless@Maxs-Air.fritz.box>
2020-09-09 11:32:32 +0200

    removed bracket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [[ -n "$SHOPIFY_LOCALE" ]]; then
+if [ -n "$SHOPIFY_LOCALE" ]; then
     echo "locale defined"
     mkdir -p settings_locale/$SHOPIFY_LOCALE
     theme get --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH/settings_locale/$SHOPIFY_LOCALE config/settings_data.json $INPUT_ARGS -e production-$SHOPIFY_LOCALE && \
