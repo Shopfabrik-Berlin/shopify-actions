16d36dd97ddcd0cc9e6657ea4b6a52778c9bd740
Max Less <maxless@Maxs-Air.fritz.box>
2020-09-15 14:20:51 +0200

    removed need for password, themeid etc

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ if [ -n "$SHOPIFY_LOCALE" ]; then
     if { [ -n "$TASK" ] && [ "$TASK" = "BACKUP" ] ;} then
     # Check task is set and back -> do Backup
         echo "Making a backup to $SHOPIFY_STORE_URL on locale: $SHOPIFY_LOCALE"
-        mkdir -p settings_locale/$SHOPIFY_LOCALE
-        theme download -e production-$SHOPIFY_LOCALE --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH/settings_locale/$SHOPIFY_LOCALE config/settings_data.json $INPUT_ARGS  && \
-        theme download -e production-$SHOPIFY_LOCALE --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH --ignored-file=config/settings_data.json $INPUT_ARGS 
+        mkdir -p $THEME_PATH/settings_locale/$SHOPIFY_LOCALE
+        theme download -e production-$SHOPIFY_LOCALE --config=config.yml --dir=$THEME_PATH/settings_locale/$SHOPIFY_LOCALE config/settings_data.json $INPUT_ARGS && \
+        theme download -e production-$SHOPIFY_LOCALE --dir=$THEME_PATH --ignored-file=config/settings_data.json $INPUT_ARGS 
     elif { [ -n "$TASK" ] && [ "$TASK" = "DEPLOY" ] ;} then
     # Check task is set and back -> do Deploy
     # Be sure to set environment i.e production, development
@@ -17,8 +17,8 @@ if [ -n "$SHOPIFY_LOCALE" ]; then
 else
     echo "locale is empty"
     if { [ -n "$TASK" ] && [ "$TASK" = "BACKUP" ] ;} then
-        echo "Making a backup to $ENVIRONMENT environment on $SHOPIFY_STORE_URL"
-        theme download -e production --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $INPUT_ARGS 
+        echo "Making a backup for $SHOPIFY_STORE_URL"
+        theme download -e production --dir=$THEME_PATH $INPUT_ARGS 
     elif { [ -n "$TASK" ] && [ "$TASK" = "DEPLOY" ] ;} then
         echo "Deploying to $ENVIRONMENT environment on $SHOPIFY_STORE_UL"
         theme deploy -e $ENVIRONMENT --dir=$THEME_PATH --allow-live --ignored-file=config/settings_data.json $INPUT_ARGS
