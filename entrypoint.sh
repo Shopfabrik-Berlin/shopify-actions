d61d5cb50b41e7fc8f127f5d02832ce918f1298f
Max Less <maxless@Maxs-Air.fritz.box>
2020-09-16 16:00:30 +0200

    removed comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,7 @@ if [ -n "$SHOPIFY_LOCALE" ]; then
         theme download --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --ignored-file=config/settings_data.json
     elif { [ -n "$TASK" ] && [ "$TASK" = "DEPLOY" ] ;} then
     # Check task is set and back -> do Deploy
-    # Be sure to set environment i.e production, development
-        echo "Deploying to $ENVIRONMENT envrionment on $SHOPIFY_LOCALE"
+        echo "Deploying $SHOPIFY_STORE_URL"
         theme deploy --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --allow-live --ignored-file=config/settings_data.json
     fi
 else
@@ -20,7 +19,7 @@ else
         echo "Making a backup for $SHOPIFY_STORE_URL"
         theme download --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID
     elif { [ -n "$TASK" ] && [ "$TASK" = "DEPLOY" ] ;} then
-        echo "Deploying to $ENVIRONMENT environment on $SHOPIFY_STORE_UL"
+        echo "Deploying $SHOPIFY_STORE_UL"
         theme deploy --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --allow-live --ignored-file=config/settings_data.json
     fi
 fi
