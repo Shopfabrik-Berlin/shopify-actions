aade8b1207a31e5a93b6e48ba8485782d1fd4ff6
Max Less <maxless@Maxs-Air.fritz.box>
2020-11-12 13:26:36 +0100

    update: new version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Check for locale
 if [ -n "$SHOPIFY_LOCALE" ]; then
+    # For Multi Store 
     echo "locale defined"
     if { [ -n "$TASK" ] && [ "$TASK" = "BACKUP" ] ;} then
     # Check task is set and back -> do Backup
@@ -12,8 +13,12 @@ if [ -n "$SHOPIFY_LOCALE" ]; then
     # Check task is set and back -> do Deploy
         echo "Deploying $SHOPIFY_STORE_URL"
         theme deploy --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --nodelete --allow-live --ignored-file=config/settings_data.json --ignored-file=locales/
+    elif { [ -n "$TASK" ] && [ "$TASK" = "PREVIEW" ] ;} then
+        echo "Creating new theme"
+        theme new --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --name="⚠[PREVIEW] - Shopfabrik $SHOPIFY_PREVIEW_NAME"
     fi
 else
+    # For Single Store 
     echo "locale is empty"
     if { [ -n "$TASK" ] && [ "$TASK" = "BACKUP" ] ;} then
         echo "Making a backup for $SHOPIFY_STORE_URL"
@@ -21,5 +26,8 @@ else
     elif { [ -n "$TASK" ] && [ "$TASK" = "DEPLOY" ] ;} then
         echo "Deploying $SHOPIFY_STORE_UL"
         theme deploy --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --nodelete --allow-live --ignored-file=config/settings_data.json --ignored-file=locales/
+    elif { [ -n "$TASK" ] && [ "$TASK" = "PREVIEW" ] ;} then
+        echo "Creating new theme"
+        theme new --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --name="⚠[PREVIEW] - Shopfabrik $SHOPIFY_PREVIEW_NAME"
     fi
 fi
