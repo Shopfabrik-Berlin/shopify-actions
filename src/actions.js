a5eb2d01222de737a2335482ce8d9628afed6ff7
AncleMarvel <anclemarvel@gmail.com@>
2022-09-26 18:18:49 +0300

    fix(deploy.clean): typeof assets

--- src/actions.js
+++ src/actions.js
@@ -55,6 +55,20 @@ async function deploy() {
  * 
  */
 async function clean() {
+    // var getAssetsConfig = {
+    //     method: 'get',
+    //     url: `https://parcel-background.myshopify.com//admin/api/2022-07/themes/134460047605/assets.json`,
+    //     headers: {'Content-Type': 'application/json', 'X-Shopify-Access-Token': `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`};
+    // };
+
+    // (function () {
+    //     if (typeof window.R === 'undefined') {
+    //       var s = document.createElement('script');
+    //       s.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js');
+    //       document.body.appendChild(s);
+    //     }
+    //   }());
+    
     var getAssetsConfig = {
         method: 'get',
         url: `https://${process.env.SHOPIFY_STORE_URL}/admin/api/2022-07/themes/${process.env.SHOPIFY_THEME_ID}/assets.json`,
@@ -73,6 +87,7 @@ async function clean() {
     console.log(assetsJSON);
 
     const assets = JSON.parse(assetsJSON);
+    console.log(typeof assets);
     const parcelFiles = assets.filter(file => file.key.includes('parcel'));
     console.log('---Parcel files:');
     console.log(parcelFiles);
