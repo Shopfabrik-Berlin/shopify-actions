a9b4bf0e889f3a44bd5bac8ee5e44fd31cf18597
Nikita Shevchenko <58083521+AncleMarvel@users.noreply.github.com>
2023-03-27 13:36:08 +0300

    feat(preview): ignore sections/*.json if !labelDev

--- src/actions.js
+++ src/actions.js
@@ -142,7 +142,7 @@ async function preview() {
     // Do no deploy if PR label contains 'X'
     if (!!!containsIgnoredLabel) {
         await deployShopifyThemeByName(name, {
-            ignoredFiles: ['templates/']
+            ignoredFiles: ['templates/', 'sections/*.json']
         })
         await deployShopifyThemeByName(name, {
             ignoredFiles: ['sections/', 'snippets/', 'locales/', 'layout/', 'config/', 'assets/']
@@ -153,13 +153,13 @@ async function preview() {
             const labelDevSplitted = labelDevLowered.split('-');
             const devThemeId = labelDevSplitted[labelDevSplitted.length - 1];
             await downloadShopifyTheme(devThemeId, {
-                ignoredFiles: ['sections/', 'snippets/', 'layout/', 'assets/']
+                ignoredFiles: ['sections/*.liquid', 'snippets/', 'layout/', 'assets/']
             }).catch((error) => {
                 console.log("Couldn't downloadShopifyTheme using this devThemeId - " + devThemeId);
                 console.log(error);
             });
             await deployShopifyThemeByName(name, {
-                ignoredFiles: ['sections/', 'snippets/', 'layout/', 'assets/']
+                ignoredFiles: ['sections/*.liquid', 'snippets/', 'layout/', 'assets/']
             }).catch((error) => {
                 console.log(error);
             });
